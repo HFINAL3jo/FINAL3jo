431ad96d119dd426a0ab48808817421e86e2092e
user2 <user2@DESKTOP-5SF3PH8>
2020-04-08 21:37:10 +0900

    444

--- src/main/java/com/aligo/spring/qna/controller/QnAController.java
+++ src/main/java/com/aligo/spring/qna/controller/QnAController.java
@@ -39,14 +39,6 @@ public class QnAController {
 		
 		return mv;
 	}
-	/*
-	 * @RequestMapping("contactView.do") public ModelAndView boardList(ModelAndView
-	 * mv) {
-	 * 
-	 * return mv; }
-	 */
->>>>>>> branch 'master' of https://github.com/HFINAL3jo/FINAL3jo.git
-	
 	
 	
 }
