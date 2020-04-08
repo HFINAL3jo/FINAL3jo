e6c6e7c6c2e9d11085449d6333f0c77efacdf668
yun_sung Yee <endlesssky0@naver.com>
2020-04-08 21:18:47 +0900

    error 수정

--- src/main/java/com/aligo/spring/member/model/dao/MemberDao.java
+++ src/main/java/com/aligo/spring/member/model/dao/MemberDao.java
@@ -1,13 +1,15 @@
 package com.aligo.spring.member.model.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.aligo.spring.member.model.vo.Member;
 
-@Repository("mDao")
+@Repository("memDao")
 public class MemberDao {
 	
+	@Autowired
 	SqlSessionTemplate sqlSession;
 
 	public int insertMember(Member m) {
--- src/main/java/com/aligo/spring/member/model/service/MemberServiceImpl.java
+++ src/main/java/com/aligo/spring/member/model/service/MemberServiceImpl.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Service;
 import com.aligo.spring.member.model.dao.MemberDao;
 import com.aligo.spring.member.model.vo.Member;
 
-@Service("mService")
+@Service("memService")
 public class MemberServiceImpl implements MemberService {
 	
 	@Autowired
-	private MemberDao mDao;
+	private MemberDao memDao;
 
 	@Override
 	public int insertMember(Member m) {
-		return mDao.insertMember(m);
+		return memDao.insertMember(m);
 	}
 	
 
