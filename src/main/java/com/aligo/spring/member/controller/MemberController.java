9fa34d6e6ea3f07a12255776e035f059786ce5b4
Gianna <Gianna@DESKTOP-3EJC05V>
2020-04-09 21:46:06 +0900

    0409 0946pm

--- src/main/java/com/aligo/spring/member/controller/MemberController.java
+++ src/main/java/com/aligo/spring/member/controller/MemberController.java
@@ -1,103 +1,46 @@
 package com.aligo.spring.member.controller;
 
-import java.util.Properties;
-import java.util.Random;
-
-import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.aligo.spring.member.model.service.MemberService;
 import com.aligo.spring.member.model.vo.Member;
-import com.google.gson.Gson;
 
+
+
+/*
+ * 회원가입
+ * → 아이디중복체크 -> 옆에 버튼만들어서 중복체크 중복이면 알럿으로 띄워주기?
+ * → 이메일 형식 지키도록 (ex. www@dfad.net 안됨) -> 인풋창 아래 올바른 형식으로 입력해주세요. 띄우기
+ * → 비밀번호 영문자+숫자 조합으로 -> 인풋창아래 영문자+숫자 조합으로 입력해주세요. 띄우기
+ * → 비밀번호 일치체크 -> 입력하신 비밀번호와 일치/불일치 합니다. 띄우기
+ * → 닉네임중복제크 -> 옆에 버튼만들어서 중복체크 중복이면 알럿으로 띄워주기?
+ * → 회원가입나이제한(0~100)
+ * → 취향선택 연결하기
+ */
 @Controller
 public class MemberController {
-	
+
 	@Autowired
 	private MemberService memService;
-	
-	
+
+
 	@RequestMapping(value="signUp.do", method = RequestMethod.POST)
 	public String insertMember(Member m) {
-		
-		
-		
-		int result = memService.insertMember(m);
-		
-		
-		/*
-		 * String host = "smtp.google.com"; String user = "noticealigo@gmail.com"; //자신의
-		 * 네이버 계정 String password = "khfinalaligo";//자신의 네이버 패스워드
-		 * 
-		 * //메일 받을 주소 String to_email = m.getEmail();
-		 * 
-		 * //SMTP 서버 정보를 설정한다. Properties props = new Properties();
-		 * props.put("mail.smtp.host", host); props.put("mail.smtp.port", 465);
-		 * props.put("mail.smtp.auth", "true"); props.put("mail.smtp.ssl.enable",
-		 * "true");
-		 * 
-		 * //인증 번호 생성기 StringBuffer temp =new StringBuffer(); Random rnd = new Random();
-		 * for(int i=0;i<10;i++) { int rIndex = rnd.nextInt(3); switch (rIndex) { case
-		 * 0: // a-z temp.append((char) ((int) (rnd.nextInt(26)) + 97)); break; case 1:
-		 * // A-Z temp.append((char) ((int) (rnd.nextInt(26)) + 65)); break; case 2: //
-		 * 0-9 temp.append((rnd.nextInt(10))); break; } } String AuthenticationKey =
-		 * temp.toString(); System.out.println(AuthenticationKey);
-		 * 
-		 * Session session = Session.getDefaultInstance(props, new
-		 * javax.mail.Authenticator() { protected PasswordAuthentication
-		 * getPasswordAuthentication() { return new
-		 * PasswordAuthentication(user,password); } });
-		 * 
-		 * //email 전송 try { MimeMessage msg = new MimeMessage(session); msg.setFrom(new
-		 * InternetAddress(user, "NEWBY 관리자"));
-		 * msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to_email));
-		 * 
-		 * // 메일 제목 msg.setSubject("Aligo 회원가입 인증 메일입니다."); // 메일 내용
-		 * msg.setText("인증 번호 : " + temp + "\n");
-		 * 
-		 * Transport.send(msg); System.out.println("이메일 전송");
-		 * 
-		 * } catch (Exception e) { e.printStackTrace();// TODO: handle exception
-		 * System.out.println("오류발생"); }
-		 * 
-		 * HttpSession saveKey = request.getSession();
-		 * saveKey.setAttribute("AuthenticationKey", AuthenticationKey);
-		 * saveKey.setAttribute("userId", to_email);
-		 * 
-		 * 
-		 * new Gson().toJson("완료", response.getWriter());
-		 * 
-		 * }
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * 
-		 * System.out.println(m);
-		 */
-		return "main";
-		
 
+		int result = memService.insertMember(m);
+		System.out.println(m);
 		
-		/*
-		 * if(result > 0) { return "redirect:main.jsp"; } else {
-		 * model.addAttribute("msg","회원 가입에 실패하셨습니다."); return "common/errorPage.jsp"; }
-		 */
-		
-		
+		if(result >0) {
+			return "redirect:index.jsp";
+		} else {
+			return "common/errorPage.jsp";
+		}
+
 	}
-	
+
 
 }
