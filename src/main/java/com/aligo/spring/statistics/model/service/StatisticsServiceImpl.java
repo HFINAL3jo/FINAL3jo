b815da79f816385801712281fe3cf83cb9e94702
ch <ch@DESKTOP-9HOMA6I>
2020-04-13 16:14:36 +0900

    2020_04_13_ch

--- src/main/java/com/aligo/spring/statistics/model/service/StatisticsServiceImpl.java
+++ src/main/java/com/aligo/spring/statistics/model/service/StatisticsServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aligo.spring.statistics.model.dao.StatisticsDao;
+import com.aligo.spring.statistics.model.vo.Statistics;
 
 @Service("serviceStatics")
 public class StatisticsServiceImpl implements StatisticsService {
@@ -14,9 +15,16 @@ public class StatisticsServiceImpl implements StatisticsService {
 	private StatisticsDao statisticsDao;
 	
 	@Override
-	public ArrayList AllGoodStatistic() {
-		
-		return statisticsDao.AllGoodStatistic();
+	public ArrayList<Statistics> AllStatistic(Statistics temp) {
+		// TODO Auto-generated method stub
+		return statisticsDao.AllStatistic(temp);
 	}
+
+	@Override
+	public ArrayList<Statistics> StatisticAjax(Statistics temp) {
+		
+		return statisticsDao.StatisticAjax(temp);
+	}
+
 
 }
